b61bdd85cdd09647350225130b9d9343e698081f
Domer79 <garipovd@mail.ru>
2016-10-22 01:32:46 +0500

    TemaplateController

---
+++ Itis.Suo/Controllers/TemplatesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Itis.Suo.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public ActionResult AdminPanel()
+        {
+            return PartialView("Security/AdminPanel");
+        }
+    }
+}
