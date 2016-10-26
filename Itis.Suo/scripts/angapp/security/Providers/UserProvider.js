ff743e05c02f218618f0e39bc69215a33c5fc76b
Domer79 <garipovd@mail.ru>
2016-10-26 22:26:24 +0500

    UserProvider.js

---
+++ Itis.Suo/scripts/angapp/security/Providers/UserProvider.js
@@ -0,0 +1,19 @@
+﻿function UserProvider() {
+    function UserFactory($http) {
+        this.getAllUsers = function () {
+            return $http.get("/Security/GetUserList").then(function (resp) {
+                return resp.data;
+            });
+        }
+    }
+
+    return {
+        $get: ['$http', function($http) {
+            return new UserFactory($http);
+        }]
+    };
+}
+
+//UserProvider.$inject = ['$http'];
+
+exports.providers.UserProvider = UserProvider;
